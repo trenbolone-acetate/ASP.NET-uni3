1b390561e83dd453c2636afb627af2e940691093
cheez <cheezballz03@gmail.com>
2024-10-20 13:02:21 +0300

    Lab4
    Introduced a LibraryController with endpoints for retrieving books and user profiles. Added models for Book and Profile, as well as a Repository and LibraryService for data management. Configured JSON file-based data storage for books and profiles.

---
+++ ASPNET3/Controllers/LibraryController.cs
@@ -0,0 +1,30 @@
+﻿using ASPNET3.Services;
+
+namespace ASPNET3.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+[Route("[controller]")]
+[ApiController]
+public class LibraryController(LibraryService libraryService) : ControllerBase
+{
+    public IActionResult Index()
+    {
+        return Ok("Hello!");
+    }
+    [HttpGet("Books")]
+    public IActionResult Books()
+    {
+        return Ok(libraryService.GetBooks());
+    }
+
+    [HttpGet("Profile/{id?}")]
+    public IActionResult Profile(int? id)
+    {
+        if (id == null)
+        {
+            return Ok($"Hi, {Environment.UserName}");
+        }
+        var profile = libraryService.GetProfile((int)id);
+        return profile != null ? Ok(profile) : Ok("Profile not found!");
+    }
+}
---
+++ ASPNET3/Models/Profile.cs
@@ -0,0 +1,7 @@
+﻿namespace ASPNET3.Models;
+
+public class Profile
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
